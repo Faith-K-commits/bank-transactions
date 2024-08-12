b0c8548751730a33f49d3282f4a30621957d1fdd
Faith Kariuki <tcwamboi@gmail.com>
2024-08-12 18:51:30 +0300

    Refactor API endpoints to use HTTPS

--- src/App.js
+++ src/App.js
@@ -5,49 +5,62 @@ import SearchBar from "./SearchBar";
 
 function App() {
   const [transactions, setTransactions] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
   useEffect(() => {
-    fetch("http://localhost:8001/transactions")
-      .then(res => res.json())
-      .then(data => setTransactions(data))
-      .catch(error => console.log(error))
-  }, [])
-  const addTransaction = newTransaction => {
-    fetch('http://localhost:8001/transactions', {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(newTransaction)
-    })
-      .then(res => res.json())
-      .then(data => setTransactions([...transactions, data]))
-  }
-  const handleSearch = term => {
+    fetch(
+      "https://my-json-server.typicode.com/Faith-K-commits/bank-transactions/transactions"
+    )
+      .then((res) => res.json())
+      .then((data) => setTransactions(data))
+      .catch((error) => console.log(error));
+  }, []);
+  const addTransaction = (newTransaction) => {
+    fetch(
+      "https://my-json-server.typicode.com/Faith-K-commits/bank-transactions/transactions",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newTransaction),
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => setTransactions([...transactions, data]));
+  };
+  const handleSearch = (term) => {
     setSearchTerm(term);
   };
-  const filteredTransactions = transactions.filter(transaction =>
+  const filteredTransactions = transactions.filter((transaction) =>
     transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
   );
-  const handleDelete = id => {
-    fetch(`http://localhost:8001/transactions/${id}`, {
-      method: "DELETE"
-    })
-      .then(response => {
+  const handleDelete = (id) => {
+    fetch(
+      `https://my-json-server.typicode.com/Faith-K-commits/bank-transactions/transactions/${id}`,
+      {
+        method: "DELETE",
+      }
+    )
+      .then((response) => {
         if (response.ok) {
-          setTransactions(transactions.filter(transaction => transaction.id !== id));
+          setTransactions(
+            transactions.filter((transaction) => transaction.id !== id)
+          );
         } else {
           throw new Error("Failed to delete transaction.");
         }
       })
-      .catch(error => {
+      .catch((error) => {
         console.log(error);
       });
-  }
+  };
   return (
     <div className="App">
       <h1>Bank Transactions</h1>
-      <TransactionList transactions={filteredTransactions} onDelete={handleDelete} />
+      <TransactionList
+        transactions={filteredTransactions}
+        onDelete={handleDelete}
+      />
       <AddTransactionForm addTransaction={addTransaction} />
       <SearchBar onSearch={handleSearch} />
     </div>
